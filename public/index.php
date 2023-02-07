a3b2d40442fcb96b5e76b01fceacf49d13308e17
ANDRIANASY <alexis.andrianasy@sogeti.com>
2023-02-07 14:59:14 +0100

    mise en place de l'application

--- public/index.php
+++ public/index.php
@@ -2,11 +2,12 @@
 
 use Router\Router;
 
-require '../vendor/autoload.php';
+require './vendor/autoload.php';
 
 define('VIEWS', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR);
 define('SCRIPTS', dirname($_SERVER['SCRIPT_NAME']) . DIRECTORY_SEPARATOR);
 
+dd($_GET['url']);
 $router = new Router($_GET['url']);
 
 $router->get('/', 'App\Controllers\BlogController@index');
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,7 +6,17 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitd87ed3ce69f5fa6a78e588c889cf9d54
 {
+    public static $files = array (
+        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
+        '667aeda72477189d0494fecd327c3641' => __DIR__ . '/..' . '/symfony/var-dumper/Resources/functions/dump.php',
+    );
+
     public static $prefixLengthsPsr4 = array (
+        'S' => 
+        array (
+            'Symfony\\Polyfill\\Mbstring\\' => 26,
+            'Symfony\\Component\\VarDumper\\' => 28,
+        ),
         'R' => 
         array (
             'Router\\' => 7,
@@ -22,6 +32,14 @@ class ComposerStaticInitd87ed3ce69f5fa6a78e588c889cf9d54
     );
 
     public static $prefixDirsPsr4 = array (
+        'Symfony\\Polyfill\\Mbstring\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-mbstring',
+        ),
+        'Symfony\\Component\\VarDumper\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/var-dumper',
+        ),
         'Router\\' => 
         array (
             0 => __DIR__ . '/../..' . '/routes',
